34d9f6254d8ff717477cd9f116bab910e254a703
robjudith <rob.judith@infiniaml.com>
2024-09-11 14:44:08 +0000

    Add some backup code to my coder-setup.sh

--- coder-setup.sh
+++ coder-setup.sh
@@ -4,6 +4,10 @@ curl -fsSL https://deb.nodesource.com/setup_22.x -o nodesource_setup.sh
 sudo -E bash nodesource_setup.sh
 sudo apt-get install -y nodejs
 sudo snap install nvim --classic
+rsync -a /data/robjudith/backup/ $HOME
+
 
 fish -c "alias fd='fdfind'; funcsave fd"
 fish -c "set -Ux PYENV_ROOT $HOME/.pyenv; set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths"
+# Set a path for custom scripts
+fish -c "set -U fish_user_paths $HOME/dotfiles/bin"
